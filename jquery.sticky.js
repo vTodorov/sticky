d0b6f79b7a33c69a6dce171740fe687ea375b49c
Vasil Todorov <vasil.todorov@provision.bg>
2018-06-29 14:26:45 +0300

    is-sticky class fix for edge scroll wheel

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -49,7 +49,7 @@
       for (var i = 0, l = sticked.length; i < l; i++) {
         var s = sticked[i],
           elementTop = s.stickyWrapper.offset().top,
-          etse = elementTop - s.topSpacing - extra;
+          etse =  parseInt(elementTop - s.topSpacing - extra);
 
         //update height in case of dynamic content
        // s.stickyWrapper.css('height', s.stickyElement.outerHeight());
